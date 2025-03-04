bbb7d1badbaa7ad1d3c837f1401bfbf36e8c38e4
jecheung038 <jecheung@ucsd.edu>
2025-03-03 22:49:11 -0800

    Class 4

---
+++ Class 4 - Copy/test.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4: R script to PDF"
+#' author: "Jacqueline Cheung (A17085191)"
+#' date: "1/16/2025"
+#' ---
+
+
+x <- 1:50
+plot(x,sin(x))
+
+# Make this nicer
+plot(x, sin(x), col= "blue", type="l", lwd = 3)
